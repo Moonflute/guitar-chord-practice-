25417316c9cfa0a49918b995dd77ff7e7921868b
Moonflute <96515859+Moonflute@users.noreply.github.com>
2026-03-04 01:25:07 +0900

    mobile

--- sw.js
+++ sw.js
@@ -1,28 +1,34 @@
-const CACHE_NAME = "guitar-v2";
+const CACHE_NAME = "guitar-v3";
 const ASSETS = [
     "./", "./index.html", "./style.css",
     "./app.js", "./fretboard.js", "./utils.js", "./sound.js",
     "./data/chords.js", "./data/scales.js",
     "./manifest.json",
-    "./icons/icon-192.png", "./icons/icon-512.png"
+    "./icons/icon.svg"
 ];
 
 self.addEventListener("install", e => {
-    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
     self.skipWaiting();
+    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
 
 self.addEventListener("activate", e => {
     e.waitUntil(
         caches.keys().then(keys =>
             Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-        )
+        ).then(() => self.clients.claim())
     );
-    self.clients.claim();
 });
 
+// Network-first: always try fresh files, fallback to cache
 self.addEventListener("fetch", e => {
     e.respondWith(
-        caches.match(e.request).then(r => r || fetch(e.request))
+        fetch(e.request)
+            .then(resp => {
+                const clone = resp.clone();
+                caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+                return resp;
+            })
+            .catch(() => caches.match(e.request))
     );
 });
