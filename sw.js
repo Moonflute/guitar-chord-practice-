e5bdb6d2d63fd77e4dc43a4d72fa0758d4a96a7c
Moonflute <96515859+Moonflute@users.noreply.github.com>
2026-03-04 02:58:55 +0900

    mobile view fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "guitar-v8";
+const CACHE_NAME = "guitar-v11";
 const ASSETS = [
     "./", "./index.html", "./style.css",
     "./app.js", "./fretboard.js", "./utils.js", "./sound.js",
